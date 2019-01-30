a84d4c70bad76dfe521290abdb26ffd1f3bb72c6
HigorSantosCIC <higorsantos.cic.unb@gmail.com>
2019-01-30 10:23:29 -0200

    update]

--- project.py
+++ project.py
@@ -65,6 +65,18 @@ class Exam():
                 )
             )
         )]
+    def getHyp(self):
+        s, d = self.systolic, self.diastolic
+        _str_ = ['Normal (ótimo)', 'Normal', 'Normal (em risco)', 'Hipertensão leve', 'Hipertensão moderada', 'Hipertensão grave']
+        return _str_[5 if s > 180 or d > 110 else (
+            4 if 180 >= s > 160 or 110 >= d > 100 else (
+                3 if 160 >= s > 140 or 100 >= d > 90 else (
+                    2 if 140 >= s > 130 or 90 >= d > 85 else (
+                        1 if 130 >= s > 120 or 85 >= d > 80 else 0
+                    )
+                )
+            )
+        )]
 
 class Patient(Person):
     def __init__(self, first_name, last_name, register, date_birth, genre, date_insurance):
@@ -102,7 +114,7 @@ class Patients():
     def addExam(self, exam):
         self.e += 1
         self.exams.append(exam)
-    def getExamByID(seld, id):
+    def getExamByID(self, id):
         return self.exams[id]
     def getPatientByID(self, id):
         return self.patients[id]
@@ -146,6 +158,7 @@ class Patients():
             'Altura (M)',
             'IMC',
             'Estado nutricional',
+            'Estado de Hipertensão',
             'Tensão arterial sistólica',
             'Tensão arterial distólica',
             'Hemodiálises',
@@ -168,6 +181,7 @@ class Patients():
             ))
         for i, e in enumerate(self.exams):
             imc = e.getIMCstr()
+            hyp = e.getHyp()
             exams.append((
                 i,
                 self.searchPatientID(e.patientRG),
@@ -177,6 +191,7 @@ class Patients():
                 e.height,
                 e.getIMC(),
                 imc,
+                hyp,
                 e.systolic,
                 e.diastolic,
                 e.hemodialysis,
@@ -189,112 +204,107 @@ class Patients():
             if imc is not 'Normal':
                 c.font = Font(bold=True, name='Arial', color='FFF00000')
                 c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
+            else:
+                c.font = Font(bold=True, name='Arial', color='006600')
+                c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
+            c = exams.cell(column=9, row=i + 2)
+            if 'Normal (ótimo)' in hyp:
+                c.font = Font(bold=True, name='Arial', color='006600')
+                c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
+            elif 'Normal (em risco)' in hyp:
+                c.font = Font(name='Arial', color='FFF00000')
+            elif 'Hipertensão leve' in hyp:
+                c.font = Font(bold=True, name='Arial', color='FFF00000')
+            elif 'Hipertensão moderada' in hyp:
+                c.font = Font(bold=True, name='Arial', color='FFF00000')
+                c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
+            elif 'Hipertensão grave' in hyp:
+                c.font = Font(bold=True, name='Arial', color='FFFFFFFF')
+                c.fill = PatternFill(fill_type='solid', start_color='cc0000', end_color='cc0000')
+            c = exams.cell(column=14, row=i + 2)
+            patient = self.searchPatient(e.patientRG)
+            if patient.genre is 'M':
+                if e.hemoglobin < 14:
+                    c.font = Font(bold=True, name='Arial', color='FFF00000')
+                    c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
+                elif 14 <= e.hemoglobin <= 18:
+                    c.font = Font(bold=True, name='Arial', color='006600')
+                    c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
+                else:
+                    c.font = Font(bold=True, name='Arial', color='FFFFFFFF')
+                    c.fill = PatternFill(fill_type='solid', start_color='cc0000', end_color='cc0000')
+            else:
+                if e.hemoglobin < 12:
+                    c.font = Font(bold=True, name='Arial', color='FFF00000')
+                    c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
+                elif 12 <= e.hemoglobin <= 16:
+                    c.font = Font(bold=True, name='Arial', color='006600')
+                    c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
+                else:
+                    c.font = Font(bold=True, name='Arial', color='FFFFFFFF')
+                    c.fill = PatternFill(fill_type='solid', start_color='cc0000', end_color='cc0000')
+            c = exams.cell(column=15, row=i + 2)
+            if 3.5 <= e.albumin <= 5:
+                c.font = Font(bold=True, name='Arial', color='FFF00000')
+                c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
+            else:
+                c.font = Font(bold=True, name='Arial', color='006600')
+                c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
+            c = exams.cell(column=16, row=i + 2)
+            if 2.5 <= e.phosphor <= 4.5:
+                c.font = Font(bold=True, name='Arial', color='FFF00000')
+                c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
+            else:
+                c.font = Font(bold=True, name='Arial', color='006600')
+                c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
+        exams.append((
+            '',
+            'MÉDIA',
+            '',
+            '',
+            '=MEDIAN(E1:E43)',
+            '=MEDIAN(F1:F43)',
+            '=MEDIAN(G1:G43)',
+            '',
+            '',
+            '=MEDIAN(J1:J43)',
+            '=MEDIAN(K1:K43)',
+            '=MEDIAN(L1:L43)',
+            '=MEDIAN(M1:M43)',
+            '=MEDIAN(N1:N43)',
+            '=MEDIAN(O1:O43)',
+            '=MEDIAN(P1:P43)'	
+        ))
+        exams.append((
+            '',
+            'DESVIO PADRÃO',
+            '',
+            '',
+            '=STDEV(E1:E43)',
+            '=STDEV(F1:F43)',
+            '=STDEV(G1:G43)',
+            '',
+            '',
+            '=STDEV(J1:J43)',
+            '=STDEV(K1:K43)',
+            '=STDEV(L1:L43)',
+            '=STDEV(M1:M43)',
+            '=STDEV(N1:N43)',
+            '=STDEV(O1:O43)',
+            '=STDEV(P1:P43)'	
+        ))
+        for cell in exams[1]:
+            cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
+            cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
+        
+        for cell in exams[44]:
+            cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
+            cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
+        for cell in exams[45]:
+            cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
+            cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
         file_1.save(peoplesName)
         file_2.save(examsName)
-
-def solveIt(data, peoples, files):
-    string_imc = [ 'Desnutrição grau V', 'Desnutrição grau IV', 'Desnutrição grau III', 'Desnutrição grau II',
-    'Desnutrição grau I', 'Normal', 'Pré-obesidade', 'Obesidade grau I', 'Obesidade grau II', 'Obesidade grau III' ]
-    today = datetime.datetime.today()
-    peoples.append((
-        'ID',
-        'Nome',
-        'Sobrenome',
-        'RG',
-        'Sexo',
-        'Data de nascimento',
-        'Idade',
-        'Peso (KG)',
-        'Altura (m)',
-        'IMC',
-        'Estado nutricional',
-    ))
-    files.append((
-        'ID',
-        'Paciente ID',
-        'Tensão arterial sistólica',
-        'Tensão arterial distólica'
-    ))
-    r_imc = []
-    i = 1
-    for row in data[1:]:
-        w = int(row[12])
-        h = round(float(int(row[13]) / 100), 2)
-        this_img = round(float(w / (h * h)), 2)
-        r_imc.append(this_img)
-        y,m,d = [int(x) for x in row[9].split('-')]
-        imc_str = 0 if this_img < 10 else (
-            1 if 10 <= this_img <= 12.9 else (
-                2 if 13 <= this_img <= 15.9 else (
-                    3 if 16 <= this_img <= 16.9 else (
-                        4 if 17 <= this_img <= 18.4 else (
-                            5 if 18.5 <= this_img <= 24.9 else (
-                                6 if 30 <= this_img <= 29.9 else (
-                                    7 if 30 <= this_img <= 34.5 else (
-                                        8 if 35 <= this_img <= 39.9 else 9
-                                    )
-                                )
-                            )
-                        )
-                    )
-                )
-            )
-        )
-        peoples.append((
-            row[0], 
-            row[4] + ' ' + row[5],
-            row[6] + ' ' + row[7],
-            row[8],
-            row[10],
-            row[9],
-            today.year - y - ((today.month, today.day) < (m, d)),
-            w,
-            h,
-            this_img,
-            string_imc[imc_str]
-        ))
-        i += 1
-        c = peoples.cell(column=11, row=i)
-        if imc_str != 5:
-            c.font = Font(bold=True, name='Arial', color='FFF00000')
-        else:
-            c.font = Font(bold=True, name='Arial', color='228B22')
-    peoples.append((
-        '',
-        'MÉDIA',
-        '',
-        '',
-        '',
-        '',
-        '=MEDIAN(G1:G43)',
-        '=MEDIAN(H1:H43)',
-        '=MEDIAN(I1:I43)',
-        '=MEDIAN(J1:J43)',
-        ''
-    ))
-    peoples.append((
-        '',
-        'DESVIO PADRÃO',
-        '',
-        '',
-        '',
-        '',
-        '=STDEV(G1:G43)',
-        '=STDEV(H1:H43)',
-        '=STDEV(I1:I43)',
-        '=STDEV(J1:J43)',
-        ''
-    ))
-    for cell in peoples[1]:
-        cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
-        cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
-    
-    for cell in peoples[44]:
-        cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
-        cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
-    for cell in peoples[45]:
-        cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
-        cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
 
 # filename = input()
 wb = load_workbook('DadosSIG.xlsx')
