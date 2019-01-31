88f51b92cbede79ca5c5e4a0d34cd2d645f94b8d
Leonardo Rodrigues <leoonardodf@gmail.com>
2019-01-31 11:07:50 -0200

    charts

--- project.py
+++ project.py
@@ -1,7 +1,12 @@
 import datetime
 from openpyxl import Workbook, load_workbook
-from openpyxl.styles import Color, PatternFill, Font, Border
-from openpyxl.styles import colors
+from openpyxl.chart import (
+    PieChart,
+    ProjectedPieChart,
+    Reference
+)
+from openpyxl.chart.series import DataPoint
+from openpyxl.styles import Color, PatternFill, Font, Border, colors
 from openpyxl.cell import Cell
 
 class Person:
@@ -45,6 +50,25 @@ class Exam():
         self.update_date = datetime.datetime.today()     
     def getIMC(self):
         return round(float(self.weight / (self.height * self.height)), 2)
+    def getIMCid(self):
+        _imc_ = self.getIMC()
+        return 0 if _imc_ < 10 else (
+            1 if 10 <= _imc_ <= 12.9 else (
+                2 if 13 <= _imc_ <= 15.9 else (
+                    3 if 16 <= _imc_ <= 16.9 else (
+                        4 if 17 <= _imc_ <= 18.4 else (
+                            5 if 18.5 <= _imc_ <= 24.9 else (
+                                6 if 30 <= _imc_ <= 29.9 else (
+                                    7 if 30 <= _imc_ <= 34.5 else (
+                                        8 if 35 <= _imc_ <= 39.9 else 9
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
     def getIMCstr(self):
         _str_ = [ 'Desnutrição grau V', 'Desnutrição grau IV', 'Desnutrição grau III', 'Desnutrição grau II', 'Desnutrição grau I', 'Normal', 'Pré-obesidade', 'Obesidade grau I', 'Obesidade grau II', 'Obesidade grau III' ]
         _imc_ = self.getIMC()
@@ -65,6 +89,17 @@ class Exam():
                 )
             )
         )]
+    def getHypID(self):
+        s, d = self.systolic, self.diastolic
+        return 5 if s > 180 or d > 110 else (
+            4 if 180 >= s > 160 or 110 >= d > 100 else (
+                3 if 160 >= s > 140 or 100 >= d > 90 else (
+                    2 if 140 >= s > 130 or 90 >= d > 85 else (
+                        1 if 130 >= s > 120 or 85 >= d > 80 else 0
+                    )
+                )
+            )
+        )
     def getHyp(self):
         s, d = self.systolic, self.diastolic
         _str_ = ['Normal (ótimo)', 'Normal', 'Normal (em risco)', 'Hipertensão leve', 'Hipertensão moderada', 'Hipertensão grave']
@@ -101,7 +136,7 @@ class Patients():
         self.patients = []
         self.exams = []
         for r in data[1:]:
-            patientID = self.addPatient(Patient(
+            self.addPatient(Patient(
                 r[4] + ' ' + r[5], r[6] + ' ' + r[7], r[8], r[9], r[10], r[11], 
             ))
             self.addExam(Exam(
@@ -133,55 +168,41 @@ class Patients():
         return -1
     def searchPatient(self, register):
         return self.getPatientByID(self.searchPatientID(register))
-    def printTable(self, peoplesName, examsName):
+    def printTable(self, peoplesName, examsName, summaryName):
         file_1 = Workbook()
         file_2 = Workbook()
+        file_3 = Workbook()
         peoples = file_1.active
         exams = file_2.active
+        summary = file_3.active
+        imc_data = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        hyp_data = [0, 0, 0, 0, 0, 0]
         peoples.append((
-            'ID',
-            'Nome',
-            'Sobrenome',
-            'RG',
-            'Sexo',
-            'Data de nascimento',
-            'Idade',
-            'Número de exames',
-            'Data de entrada no seguro'
+            'ID', 'Nome', 'Sobrenome', 'RG', 'Sexo', 'Data de nascimento', 'Idade', 'Número de exames', 'Data de entrada no seguro'
         ))
         exams.append((
-            'ID',
-            'Paciente ID',
-            'Auditor',
-            'Cargo do auditor',
-            'Peso (KG)',
-            'Altura (M)',
-            'IMC',
-            'Estado nutricional',
-            'Estado de Hipertensão',
-            'Tensão arterial sistólica',
-            'Tensão arterial distólica',
-            'Hemodiálises',
-            'Dosis Dialisis (KT/V)',
-            'Hemoglobina',
-            'Albúmina sérica',
-            'Fósforo'
+            'ID', 'Paciente ID', 'Auditor', 'Cargo do auditor', 'Peso (KG)', 'Altura (M)', 'IMC', 'Estado nutricional',
+            'Estado de Hipertensão', 'Hemoglobina', 'Albúmina sérica', 'Fósforo'
         ))
         for i, u in enumerate(self.patients):
             peoples.append((
-                i,
-                u.first_name,
-                u.last_name,
-                u.register,
-                u.genre,
-                u.date_birth,
-                u.getAge(),
-                u.exams,
+                i, 
+                u.first_name, 
+                u.last_name, 
+                u.register, 
+                u.genre, 
+                u.date_birth, 
+                u.getAge(), 
+                u.exams, 
                 u.date_insurance
             ))
+
         for i, e in enumerate(self.exams):
             imc = e.getIMCstr()
             hyp = e.getHyp()
+            patient = self.searchPatient(e.patientRG)
+            imc_data[e.getIMCid()] += 1
+            hyp_data[e.getHypID()] += 1
             exams.append((
                 i,
                 self.searchPatientID(e.patientRG),
@@ -192,10 +213,6 @@ class Patients():
                 e.getIMC(),
                 imc,
                 hyp,
-                e.systolic,
-                e.diastolic,
-                e.hemodialysis,
-                e.dialyses,
                 e.hemoglobin,
                 e.albumin,
                 e.phosphor
@@ -212,17 +229,21 @@ class Patients():
                 c.font = Font(bold=True, name='Arial', color='006600')
                 c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
             elif 'Normal (em risco)' in hyp:
-                c.font = Font(name='Arial', color='FFF00000')
+                c.font = Font(name='Arial', color='ff7500')
+                c.fill = PatternFill(fill_type='solid', start_color='fff3df', end_color='fff3df')
+            elif 'Normal' in hyp:
+                c.font = Font(name='Arial', color='006600')
+                c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
             elif 'Hipertensão leve' in hyp:
                 c.font = Font(bold=True, name='Arial', color='FFF00000')
+                c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
             elif 'Hipertensão moderada' in hyp:
                 c.font = Font(bold=True, name='Arial', color='FFF00000')
                 c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
             elif 'Hipertensão grave' in hyp:
                 c.font = Font(bold=True, name='Arial', color='FFFFFFFF')
                 c.fill = PatternFill(fill_type='solid', start_color='cc0000', end_color='cc0000')
-            c = exams.cell(column=14, row=i + 2)
-            patient = self.searchPatient(e.patientRG)
+            c = exams.cell(column=10, row=i + 2)
             if patient.genre is 'M':
                 if e.hemoglobin < 14:
                     c.font = Font(bold=True, name='Arial', color='FFF00000')
@@ -243,20 +264,21 @@ class Patients():
                 else:
                     c.font = Font(bold=True, name='Arial', color='FFFFFFFF')
                     c.fill = PatternFill(fill_type='solid', start_color='cc0000', end_color='cc0000')
-            c = exams.cell(column=15, row=i + 2)
+            c = exams.cell(column=11, row=i + 2)
             if 3.5 <= e.albumin <= 5:
                 c.font = Font(bold=True, name='Arial', color='FFF00000')
                 c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
             else:
                 c.font = Font(bold=True, name='Arial', color='006600')
                 c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
-            c = exams.cell(column=16, row=i + 2)
+            c = exams.cell(column=12, row=i + 2)
             if 2.5 <= e.phosphor <= 4.5:
                 c.font = Font(bold=True, name='Arial', color='FFF00000')
                 c.fill = PatternFill(fill_type='solid', start_color='ffcccc', end_color='ffcccc')
             else:
                 c.font = Font(bold=True, name='Arial', color='006600')
                 c.fill = PatternFill(fill_type='solid', start_color='ccffcc', end_color='ccffcc')
+
         exams.append((
             '',
             'MÉDIA',
@@ -269,11 +291,7 @@ class Patients():
             '',
             '=MEDIAN(J1:J43)',
             '=MEDIAN(K1:K43)',
-            '=MEDIAN(L1:L43)',
-            '=MEDIAN(M1:M43)',
-            '=MEDIAN(N1:N43)',
-            '=MEDIAN(O1:O43)',
-            '=MEDIAN(P1:P43)'	
+            '=MEDIAN(L1:L43)'
         ))
         exams.append((
             '',
@@ -287,24 +305,82 @@ class Patients():
             '',
             '=STDEV(J1:J43)',
             '=STDEV(K1:K43)',
-            '=STDEV(L1:L43)',
-            '=STDEV(M1:M43)',
-            '=STDEV(N1:N43)',
-            '=STDEV(O1:O43)',
-            '=STDEV(P1:P43)'	
+            '=STDEV(L1:L43)'	
         ))
+        for cell in peoples[1]:
+            cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
+            cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
+
         for cell in exams[1]:
             cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
             cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
-        
+                
         for cell in exams[44]:
             cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
             cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
         for cell in exams[45]:
             cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
             cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
+
         file_1.save(peoplesName)
         file_2.save(examsName)
+
+        _str_ = [ 'Desnutrição grau V', 'Desnutrição grau IV', 'Desnutrição grau III', 'Desnutrição grau II', 'Desnutrição grau I', 'Normal', 'Pré-obesidade', 'Obesidade grau I', 'Obesidade grau II', 'Obesidade grau III' ]
+        summary.append((
+            'Estado nutricional', 'Número de pacientes'
+        ))
+        max, id = 0, 0
+        for i, x in enumerate(_str_):
+            summary.append((
+                x, imc_data[i]
+            ))
+            if imc_data[i] > max:
+                id = i
+                max = imc_data[i]
+        pie = PieChart()
+        labels = Reference(summary, min_col=1, min_row=2, max_row=11)
+        data = Reference(summary, min_col=2, min_row=1, max_row=11)
+        pie.add_data(data, titles_from_data=True)
+        pie.set_categories(labels)
+        pie.title = "Distribuição nutricional"
+        slice = DataPoint(idx=id, explosion=10)
+        pie.series[0].data_points = [slice]
+        pie.height = 6
+        summary.add_chart(pie, "C1")
+
+        summary.append([('')])
+        summary.append([('')])
+        summary.append((
+            'Hipertensão', 'Número de pacientes'
+        ))
+
+        _str_ = ['Normal (ótimo)', 'Normal', 'Normal (em risco)', 'Hipertensão leve', 'Hipertensão moderada', 'Hipertensão grave']
+        max, id = 0, 0
+        for i, x in enumerate(_str_):
+            summary.append((
+                x, hyp_data[i]
+            ))
+            if hyp_data[i] > max:
+                id = i
+                max = hyp_data[i]
+        pie2 = PieChart()
+        labels = Reference(summary, min_col=1, min_row=15, max_row=20)
+        data = Reference(summary, min_col=2, min_row=14, max_row=20)
+        pie2.add_data(data, titles_from_data=True)
+        pie2.set_categories(labels)
+        pie2.title = "Distribuição de hipertensão"
+        slice = DataPoint(idx=id, explosion=10)
+        pie2.series[0].data_points = [slice]
+        pie2.height = 6
+        summary.add_chart(pie2, "C14")
+
+        for cell in summary[1]:
+            cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
+            cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
+        for cell in summary[14]:
+            cell.fill = PatternFill(start_color="aabedd", end_color="aabedd", fill_type = "solid")
+            cell.font = Font(bold=True, name='Arial', color='FFFFFFFF')
+        file_3.save(summaryName)
 
 # filename = input()
 wb = load_workbook('DadosSIG.xlsx')
@@ -317,4 +393,4 @@ for row in ws:
     all_rows.append(current_row)
 
 patients = Patients(all_rows)
-patients.printTable('Pessoas.xlsx', 'Ficha.xlsx')
+patients.printTable('Pessoas.xlsx', 'Ficha.xlsx', 'Relatório.xlsx')
