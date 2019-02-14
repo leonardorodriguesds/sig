e83dc4e101616f1ae5dcb7cc02a1cf4bd7e349ea
henrique-mariano <henrique_mariano@aluno.unb.br>
2019-02-14 19:37:06 -0200

    Inserção dos dados no bd

--- projectbd.py
+++ projectbd.py
@@ -1,4 +1,6 @@
 import sqlite3
+import openpyxl
+from openpyxl import Workbook, load_workbook
 
 class SQLCommand:
     def __init__(self):
@@ -11,6 +13,12 @@ class SQLCommand:
         self.connect()
         self._cursor = self._conn.cursor()
         self._cursor.execute(command)
+        self.desconnect()
+    def executemany(self, command, rows_list):
+        self.connect()
+        self._cursor = self._conn.cursor()
+        self._cursor.executemany(command, rows_list)
+        self._conn.commit()
         self.desconnect()
 
 class CreateTablePerson:
@@ -53,6 +61,25 @@ class CreateTableRecord:
     def getCommand(self):
         return self.command
 
+class FillPersonTable:
+    def __init__(self):
+        self.command = """
+        INSERT INTO person (id, first_name, second_name, register, genre, date_birth, age, number, date_insurance) 
+        VALUES (?,?,?,?,?,?,?,?,?)
+        """
+    def getCommand(self):
+        return self.command
+
+class FillRecordTable:
+    def __init__(self):
+        self.command = """
+        INSERT INTO record (id, pacient_id, auditor, auditor_position, weight, height, imc, nutritional_state, hypertension_state, hemoglobin, albumin, phosphor) 
+        VALUES (?,?,?,?,?,?,?,?,?,?,?,?)
+        """
+    def getCommand(self):
+        return self.command
+
+
 if __name__ == "__main__":
     TabelaPessoa = CreateTablePerson()
     TabelaFicha = CreateTableRecord()
@@ -60,3 +87,31 @@ if __name__ == "__main__":
 
     Executar.execute(TabelaPessoa.getCommand())
     Executar.execute(TabelaFicha.getCommand())
+
+    wb = load_workbook('Pessoas.xlsx')
+    ws = wb.get_sheet_by_name('Sheet')
+    all_rows = []
+
+    for row in ws:
+        current_row = []
+        for cell in row:
+            current_row.append(cell.value)
+        all_rows.append(current_row)
+
+    Preencher_pessoa = FillPersonTable()
+    Executar.executemany(Preencher_pessoa.getCommand(), all_rows[1:])
+    
+    wb = load_workbook('Ficha.xlsx')
+    ws = wb.get_sheet_by_name('Sheet')
+    all_rows = []
+
+    for row in ws:
+        current_row = []
+        for cell in row:
+            current_row.append(cell.value)
+        all_rows.append(current_row)
+
+    Preencher_ficha = FillRecordTable()
+    Executar.executemany(Preencher_ficha.getCommand(), all_rows[1 : -2])
+
+    print('Dados inseridos com sucesso.')
